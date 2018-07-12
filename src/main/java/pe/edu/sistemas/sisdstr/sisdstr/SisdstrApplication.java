63acf3c1875d087eda059204deac4236a884b608
HenryJCM <henjacolonia17@gmail.com>
2018-07-12 01:08:27 -0500

    iniciando proyecto

---
+++ src/main/java/pe/edu/sistemas/sisdstr/sisdstr/SisdstrApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.sistemas.sisdstr.sisdstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisdstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisdstrApplication.class, args);
+	}
+}
